18ddb779c3d8efbe20a59e48edf3af5fb0ca4d36
RIMikeC <mike.charles@river-island.com>
2018-06-08 12:16:26 +0100

    example Python collection strategies

--- terraform/modules/lambda_audit_aws/source/audit_aws.py
+++ terraform/modules/lambda_audit_aws/source/audit_aws.py
@@ -11,6 +11,7 @@
 import boto3
 import sys
 import datetime
+import json
 
 try:
     ec2=boto3.client('ec2',region_name = 'eu-west-1')
@@ -18,10 +19,39 @@ except Exception as e:
     print("ERROR: failed to connect to EC2 client")
     sys.exit(1)
 
+try:
+    asg=boto3.client('autoscaling',region_name = 'eu-west-1')
+except Exception as e:
+    print("ERROR: failed to connect to ASG client")
+    sys.exit(1)
+
 #def lambda_handler(event, context):  
-todays_date=datetime.date.today()
+#todays_date=datetime.date.today()
+
 response=ec2.describe_instances()
-print("ID,AMI,Size,Public IP,Private IP")
-for instance in response['Reservations'][0]['Instances']:
-    print(instance['InstanceId'],instance['ImageId'],instance['InstanceType'],instance['PublicIpAddress'],instance['PrivateIpAddress'],sep=',')
+print("ID,AMI,Size")
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        print(instance['InstanceId'],instance['ImageId'],instance['InstanceType'],sep=',')
+
+print()
+
+response=ec2.describe_instances()
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        print(json.dumps({instance['InstanceId']:[{'AMI':instance['ImageId'],'Size':instance['InstanceType']}]}, indent=4))
+
+print()
+
+response=asg.describe_auto_scaling_groups()
+print("ASG,Min,Max,Desired,Current")
+for group in response['AutoScalingGroups']:
+    print(group['AutoScalingGroupName'],group['MinSize'],group['MaxSize'],group['DesiredCapacity'],len(group['Instances']),sep=',')
+
+print()
+
+response=asg.describe_auto_scaling_groups()
+for group in response['AutoScalingGroups']:
+    print(json.dumps({group['AutoScalingGroupName']:[{'Min':group['MinSize'],'Max':group['MaxSize'],'Desired':group['DesiredCapacity'],'Current':len(group['Instances'])}]}, indent=4))
+
 
